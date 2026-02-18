e7fbf0dd10629f9ff97fba6c48ba7d2f102d907a
pradeepkumar <pradeepchaudhary1997@gmail.com>
2026-02-18 22:06:28 +0530

    Mobile version 1 is ready

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
